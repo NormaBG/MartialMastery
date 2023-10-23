7901875138edbe39e91bd0d3db62252fd81c03ba
Norma Bojorquez <norma_rara26@hotmail.com>
2023-10-23 16:10:18 -0700

    Generacion de Token (error)

---
+++ webapi/Controllers/AuthenticationController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapi.Models;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+
+
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+
+        private readonly string secretKey; 
+        
+
+        public AuthenticationController(IConfiguration config)
+        {
+            secretKey = config.GetSection("settings").GetSection("secretkey").ToString();
+        }
+
+        [HttpPost]
+        [Route("Validar")]
+        public async Task<IActionResult> Validar([FromBody] Usuario request)
+        {
+            using (var contextobd = new MartialMasterContext())
+            {
+                var usuarioEncontrado = await contextobd.Usuarios.SingleOrDefaultAsync(u => u.Usuario1 == request.Usuario1);
+                var contraencontrada = await contextobd.Usuarios.SingleOrDefaultAsync(u => u.Contrasena == request.Contrasena);
+                
+                if (usuarioEncontrado != null && contraencontrada != null)
+                {
+                    var keyBytes = Encoding.ASCII.GetBytes(secretKey);
+                    var claims = new ClaimsIdentity();
+                    claims.AddClaim(new Claim(ClaimTypes.NameIdentifier, request.Usuario1));
+
+                    //creartoken
+
+                    var tokenDescriptor = new SecurityTokenDescriptor
+                    {
+                        //propiedades del token
+                        Subject = claims,
+                        Expires = DateTime.UtcNow.AddMinutes(5),
+                        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(keyBytes),SecurityAlgorithms.HmacSha256Signature)
+                    };
+
+                    //lectura del token
+
+                    var tokenHandler = new JwtSecurityToken();
+                    var tokenConfig = tokenHandler.CreateToken(tokenDescriptor);
+
+                    string tokencreador = tokenHandler.WriteToken(tokenConfig);
+
+                    return StatusCode(StatusCodes.Status200OK, new { token = tokencreador});
+                }
+                else
+                {
+                    return StatusCode(StatusCodes.Status401Unauthorized, new { token = "" });
+                }
+            }
+        }
+    }
+}
--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,12 +1,38 @@
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 using webapi.Models;
 //using.webapi.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//OBTENER LLAVE SECRETA Y CONVERTIRLO EN BYTE / cCONFIGURACION PARA IMPLEMENTAR JSON WEB TOKEN
+builder.Configuration.AddJsonFile("appsettings.json");
+var secretkey = builder.Configuration.GetSection("setting").GetSection("proyectoW3b").ToString();
+var keyBytes = Encoding.UTF8.GetBytes(secretkey);
+
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+
+}).AddJwtBearer(config =>
+{
+    config.RequireHttpsMetadata = false;
+    config.SaveToken = true;
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -40,10 +66,11 @@ app.UseCors(options =>
     options.AllowAnyHeader();
 });
 
-//agregar usuario
 
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
