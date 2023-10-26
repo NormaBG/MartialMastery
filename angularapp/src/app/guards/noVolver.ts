953a0939f6336e2aa7c6848e20eb73ffdec3ded3
Norma Bojorquez <norma_rara26@hotmail.com>
2023-10-25 23:54:14 -0700

    Cambie el nombre de una funcion para hacer mas entendible el codigo

--- angularapp/src/app/guards/noVolver.ts
+++ angularapp/src/app/guards/noVolver.ts
@@ -1,17 +1,17 @@
 // auth.guard.ts
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Injectable()
-export class noVolver implements CanActivate {
+export class noVolver{
   constructor(private router: Router) { }
 
-  canActivate(): boolean {
+  NoVolver (): boolean {
     // comprueba si el usuario a iniciado sesion
     if (localStorage.getItem('token')) {
       this.router.navigate(['/dashboard']); // Redirige al panel de control si el usuario ya está autenticado.
       return false; // No permite el acceso a la página de inicio de sesión.
     }
-    return true; // Permite el acceso a la página de inicio de sesión si el usuario no ha iniciado sesión.
+    return true; 
   }
 }
